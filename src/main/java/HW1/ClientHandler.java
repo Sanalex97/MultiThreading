d2f90270a6d3e296f095eb129a86a5b7a8ff01f7
Alexander Krasyukov <uponef64@gmail.com>
2022-12-18 15:59:48 +0300

    Добавлены обработчики запросов

--- src/main/java/HW1/ClientHandler.java
+++ src/main/java/HW1/ClientHandler.java
@@ -2,18 +2,12 @@ package HW1;
 
 import java.io.*;
 import java.net.Socket;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.List;
 
-public class ClientHandler implements Runnable{
+
+public class ClientHandler implements Runnable {
     private Socket clientSocket;
     private BufferedReader in;
     private BufferedOutputStream out;
-
-    private final List<String > validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
 
     public ClientHandler(Socket socket) {
         this.clientSocket = socket;
@@ -40,65 +34,42 @@ public class ClientHandler implements Runnable{
 
     @Override
     public void run() {
-        while (true) {
+        while (!clientSocket.isClosed()) {
             try {
                 final String requestLine = in.readLine();
+
+                if (requestLine == null) {
+                    continue;
+                }
+
                 final String[] parts = requestLine.split(" ");
 
+                System.out.println(requestLine);
 
                 if (parts.length != 3) {
                     // just close socket
                     continue;
                 }
 
-                final String path = parts[1];
-                if (!validPaths.contains(path)) {
-                    out.write((
-                            "HTTP/1.1 404 Not Found\r\n" +
-                                    "Content-Length: 0\r\n" +
-                                    "Connection: close\r\n" +
-                                    "\r\n"
-                    ).getBytes());
-                    out.flush();
-                    continue;
+                Request request = new Request(parts[0], parts[1]);
+
+                synchronized (Server.handlers) {
+                    try {
+                        Server.handlers.get(request.getMethod()).get("/messages").handle(request, out);
+                    } catch (NullPointerException ex) {
+                        out.write((
+                                "HTTP/1.1 404 Not Found\r\n" +
+                                        "Content-Length: 0\r\n" +
+                                        "Connection: close\r\n" +
+                                        "\r\n"
+                        ).getBytes());
+                    }
                 }
-
-                final Path filePath = Path.of(".", "public", path);
-                final String mimeType = Files.probeContentType(filePath);
-
-                // special case for classic
-                if (path.equals("/classic.html")) {
-                    final String template = Files.readString(filePath);
-                    final byte[] content = template.replace(
-                            "{time}",
-                            LocalDateTime.now().toString()
-                    ).getBytes();
-                    out.write((
-                            "HTTP/1.1 200 OK\r\n" +
-                                    "Content-Type: " + mimeType + "\r\n" +
-                                    "Content-Length: " + content.length + "\r\n" +
-                                    "Connection: close\r\n" +
-                                    "\r\n"
-                    ).getBytes());
-                    out.write(content);
-                    out.flush();
-                    continue;
-                }
-
-                final long length = Files.size(filePath);
-                out.write((
-                        "HTTP/1.1 200 OK\r\n" +
-                                "Content-Type: " + mimeType + "\r\n" +
-                                "Content-Length: " + length + "\r\n" +
-                                "Connection: close\r\n" +
-                                "\r\n"
-                ).getBytes());
-                Files.copy(filePath, out);
-                out.flush();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
+
 }
 
