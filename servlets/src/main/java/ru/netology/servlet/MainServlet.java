b953df049a5abea8b0218d3bd6d0061b9d43c260
Alexander Krasyukov <uponef64@gmail.com>
2023-01-15 11:21:07 +0300

    Реализация с JavaConfig

--- servlets/src/main/java/ru/netology/servlet/MainServlet.java
+++ servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -22,7 +22,7 @@ public class MainServlet extends HttpServlet {
   @Override
   public void init() {
     final var context = new AnnotationConfigApplicationContext(JavaConfig.class);
-    controller = (PostController) context.getBean("postController");
+    controller = context.getBean(PostController.class);
   }
 
   @Override
